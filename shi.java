82f930b48b242c0b86f8acf142eead5125e5a4a0
jv2404 <jigarvekariya2404@gmail.com>
2023-01-27 09:56:00 +0530

    hello

---
+++ shi.java
@@ -0,0 +1,12 @@
+public class shi {
+
+    public static void main(String[] args) {
+
+        char c = 'S';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println( " is an alphabet." +c);
+        else
+            System.out.println (" is not an alphabet." +c);
+    }
+}
